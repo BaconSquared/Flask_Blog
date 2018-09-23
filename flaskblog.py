5138dfa4ad13f2761aaa08f10303613c6b48ec43
BaconSquared <matt.klocek@gmail.com>
2018-09-23 14:27:29 -0500

    Add layout template and include bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -2,15 +2,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Matt Klocek',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 23, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 24, 2018'
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 # if __name__ == '__main__':
